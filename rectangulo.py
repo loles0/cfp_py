366f5b6558565cc207cf4a7f0b075562399017ed
maiton <miltonschwindt@gmail.com>
2026-05-06 16:26:49 +0200

    rectangulo

---
+++ rectangulo.py
@@ -0,0 +1,6 @@
+print ("ingrese la base y altura del rectangulo y sabra la superficie y perimetro")
+BASE=float(input("ingrese la base: "))
+ALTO=float(input("ingrese la altura: "))
+SUP=BASE*ALTO
+PER=2*(BASE+ALTO)
+print ("Superficie: ",SUP," perimetro: ",PER)
